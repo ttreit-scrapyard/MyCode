a4ce60d245e19a4e4669f0d2b964ad19ca1d31ee
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-02 03:03:26 -0700

    fixed error

--- fstring.py
+++ fstring.py
@@ -4,6 +4,6 @@ color = 'hazel'
 
 print('Hi {} you have {} eyes.'.format(name, color))
 
-It's not terrible but f strings make it a lot more readable.
+# It's not terrible but f strings make it a lot more readable.
 
 print(f'Hi {name} you have {color} eyes.')
